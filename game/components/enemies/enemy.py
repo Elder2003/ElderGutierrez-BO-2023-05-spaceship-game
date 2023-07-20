60d6ca5bf7b796df6deae9c336804b29f4fd1eaf
Elder2003 <hamergut2003@gmail.com>
2023-07-19 23:21:16 -0400

    Class 3
    Tuve dificultad al implementar el código nuevo

--- game/components/enemies/enemy.py
+++ game/components/enemies/enemy.py
@@ -3,7 +3,8 @@ import random
 from pygame.sprite import Sprite
 
 from game.utils.constants import ENEMY_1, SCREEN_HEIGHT, SCREEN_WIDTH
-
+from game.components.bullets.bullet import Bullet
+ 
 class Enemy(Sprite):
     ENEMY_WIDTH = 40
     ENEMY_HEIGHT = 60
@@ -12,7 +13,9 @@ class Enemy(Sprite):
     SPEED_ON_Y = 1
     SPEED_ON_X = 10
     MOVES = { 0: 'left', 1: 'right' }
-
+    INITIAL_SHOOTING_TIME = 1000
+    FINAL_SHOOTING_TIME = 3000
+    
     def __init__(self):
         self.image = ENEMY_1
         self.image = pygame.transform.scale(self.image, (self.ENEMY_WIDTH, self.ENEMY_HEIGHT))
@@ -20,9 +23,14 @@ class Enemy(Sprite):
         self.direction = self.MOVES[random.randint(0, 1)]
         self.movement_count = 0
         self.moves_before_change = random.randint(20, 50)
+        self.type = 'enemy'
+        current_time = pygame.time.get_ticks()
+        self.shooting_time = random.randint(current_time + self.INITIAL_SHOOTING_TIME, current_time + self.INITIAL_SHOOTING_TIME)
+        
 
     def update(self, enemies):
         self.rect.y += self.SPEED_ON_Y
+        self.shoot(game.bullet_manager)
 
         if self.direction == self.MOVES[0]:
             self.rect.x -= self.SPEED_ON_X
@@ -47,3 +55,11 @@ class Enemy(Sprite):
 
         if (self.movement_count >= self.moves_before_change):
             self.movement_count = 0
+
+    def shoot(self, bullet_manager):
+        current_time = pygame.time.get_ticks()
+
+        if self.shooting_time <= current_time:
+            bullet = Bullet(self)
+            bullet_manager.add_bullet(bullet)
+            self.shooting_time += random.randint(self.INITIAL_SHOOTING_TIME, self.FINAL_SHOOTING_TIME)        
